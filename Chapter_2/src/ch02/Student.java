98604944c1f40c5fa7a9c7f90f79896082555589
Lukaid <crescent3859@gmail.com>
2021-08-09 20:11:30 +0900

    0809

---
+++ Chapter_2/src/ch02/Student.java
@@ -0,0 +1,13 @@
+package ch02;
+
+// 한 클래스 파일에서 여러개의 클래스를 가질 수 있지만
+// public은 하나이고 그 이름은 파일의 이름과 동일해야 됨
+
+public class Student {
+
+	int studentNumber; // 멤버변수
+	String studentName;
+	int majorCode;
+	String majorName;
+	int grade;
+}
